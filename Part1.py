cfc6e13de8fbf7c8295fdfe26ffbd4c09110b2da
Aldo Sanchez <52171931+aldosanchez2002@users.noreply.github.com>
2023-11-06 10:08:05 -0700

    UR Algorithm and parameters to each Algorithm

--- Part1.py
+++ Part1.py
@@ -1,15 +1,70 @@
 import sys
+import numpy as np
 
 def uniformRandom(parameter, turn, board):
-    pass
+    '''
+    This is a trivial algorithm used for basic testing and benchmarking. It selects a legal 
+    move for the specified player using the uniform random strategy (i.e., each legal 
+    move is selected with the same probability. The parameter value should always be 0 
+    for this algorithm. 
+    '''
+    possible_moves = [i for i in range(len(board[0])) if board[0][i] == 'O']
+    np.random.shuffle(possible_moves)
+    return possible_moves[0]
 
 def depthLimitedMinMax(parameter, turn, board):
+    '''
+    This algorithm uses depth-first minmax search out to a specified  maximum depth to 
+    select a move. You should test each node to see whether it is a terminal state (i.e., a 
+    win for one player or a draw); if so return that value immediately. If you reach the 
+    depth limit without reaching a terminal state, apply a heuristic evaluation function 
+    to the game state and return that value. You may choose to implement any 
+    reasonable heuristic; you will give a brief description in your report. The heuristic 
+    should take in any valid (non-terminal) board state as input and return a predicted 
+    value for that state between -1 and 1. For example, you may want to count the 
+    number of lines of 2 or 3 pieces that can be extended for each player. If there are 
+    ties, break them randomly. Make sure you keep track of which player (maximizing 
+    or minimizing) is making the choice at each node. Output the value for each of the 
+    immediate next moves from the root (with Null for illegal moves) and the final move 
+    selected. Example in Instructions.pdf
+    '''
     pass
 
 def pureMonteCarloGameSearch(parameter, turn, board):
+    '''
+    This algorithm is the simplest form of game tree search based on randomized 
+    rollouts. It is essentially the UCT algorithm without a sophisticated tree search 
+    policy. Please refer to https://en.wikipedia.org/wiki/Monte_Carlo_tree_search for 
+    detailed descriptions and examples for both PMCGS and UCT (of course you can also 
+    use the course slides, textbook, and other references as needed). The main steps in 
+    this algorithm are the same as in UCT, but every move both within the tree search 
+    and the rollout is made at random. Output the value for each of the immediate next 
+    moves (with Null for illegal moves) and the move selected at the end. Only if the 
+    “Verbose” mode is selected you should also print out additional information during 
+    each simulation trace, as shown below. For each node in the search tree output the 
+    current values of wi and ni, and the move selected. When you reach a leaf in the 
+    current tree and add a new node print “NODE ADDED”. For the rollout print only the 
+    moves selected, and when you reach a terminal node print the value as “TERMINAL 
+    NODE VALUE: X” where X is -1, 0, or 1. Then print the updated values. Example in 
+    Instructions.pdf
+    '''
     pass
 
 def upperConfidenceBound(parameter, turn, board):
+    '''
+    Builds on PMCGS and uses most of the same structure. The only 
+    difference is in how nodes are selected within the existing search tree; instead of 
+    selecting randomly the nodes are selected using the Upper Confidence Bounds 
+    (UCB) algorithm. For any node that is NOT a leaf (i.e., all possible children are 
+    already in the tree), calculate the UCB value for all children, and pick the one with 
+    the highest (or lowest) value depending on which player is choosing a move. The 
+    output should be mostly the same as for PMCGS, but adds the UCB values computed 
+    for the children before specifying the move that is selected for the tree search part 
+    of the simulation when in “Verbose” mode. Note that the final values printed for the 
+    children of the root and the final move selection are NOT based on the UCB 
+    equation, but the direct estimate of the node value (i.e., just wi/ni). Example in 
+    Instructions.pdf
+    '''
     pass
 
 def injestTestFile(input_file):
@@ -62,5 +117,3 @@ if __name__ == "__main__":
         sys.exit(1)
 
     print(f"Final Move Selected: {output}")
-    
-    
